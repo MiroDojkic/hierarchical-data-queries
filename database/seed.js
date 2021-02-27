2a99a32b43272e22e32ce752fe41bae26c93e402
Miro Dojkic <mdojkic@gmail.com>
2021-02-27 13:01:36 +0100

    Enable seeding arbitrary number of rows ⚡️

--- database/seed.js
+++ database/seed.js
@@ -1,19 +1,22 @@
+const chunk = require('lodash/chunk');
 const faker = require('faker');
 const times = require('lodash/times');
 const flatten = require('lodash/flatten');
 const { db } = require('.');
 
-const BATCH_INSERT_SIZE = 500;
 async function seed(sizePerLevel, levels) {
-  async function seedSubordinates(superiorIds, currentLevel) {
+  async function seedEmployees(superiorIds, currentLevel) {
     if (currentLevel > levels) return;
     const employees = flatten(superiorIds.map(createSubordinates));
-    const subordinateIds = await db
-      .batchInsert('employees', employees, BATCH_INSERT_SIZE)
-      .returning('id');
-    return seedSubordinates(subordinateIds, currentLevel + 1);
+    const subordinateIds = [];
+    const employeeChunks = chunk(employees, 1000);
+    for (let i = 0; i < employeeChunks.length; i++) {
+      const ids = await db('employees').insert(employeeChunks[i]).returning('id');
+      subordinateIds.push(...ids);
+    }
+    return seedEmployees(subordinateIds, currentLevel + 1);
   }
-  return seedSubordinates([null], 1);
+  return seedEmployees([null], 1);
 
   function createSubordinates(superiorId) {
     return times(sizePerLevel, () => ({
